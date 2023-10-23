d7c5110962954b675c7d0dfda17fd76fa2f6a074
LFGM14 <74067091+LFGM14@users.noreply.github.com>
2023-10-22 22:08:48 -0500

    switch agregado

--- ConsoleApplication6/ConsoleApplication6.cpp
+++ ConsoleApplication6/ConsoleApplication6.cpp
@@ -18,7 +18,55 @@ void setxy(float x, int y) {
 float aleatorio() {
     return (float)rand() / RAND_MAX;
 }
-
+void CambiarColor(int x) {
+    switch (x) {
+    case 0:
+        Console::ForegroundColor = ConsoleColor::White;
+        break;
+    case 1:
+        Console::ForegroundColor = ConsoleColor::Yellow;
+        break;
+    case 2:
+        Console::ForegroundColor = ConsoleColor::Blue;
+        break;
+    case 3:
+        Console::ForegroundColor = ConsoleColor::Red;
+        break;
+    case 4:
+        Console::ForegroundColor = ConsoleColor::Green;
+        break;
+    case 5:
+        Console::ForegroundColor = ConsoleColor::DarkBlue;
+        break;
+    case 6:
+        Console::ForegroundColor = ConsoleColor::DarkGreen;
+        break;
+    case 7:
+        Console::ForegroundColor = ConsoleColor::DarkCyan;
+        break;
+    case 8:
+        Console::ForegroundColor = ConsoleColor::DarkRed;
+        break;
+    case 9:
+        Console::ForegroundColor = ConsoleColor::DarkMagenta;
+        break;
+    case 10:
+        Console::ForegroundColor = ConsoleColor::DarkYellow;
+        break;
+    case 11:
+        Console::ForegroundColor = ConsoleColor::Gray;
+        break;
+    case 12:
+        Console::ForegroundColor = ConsoleColor::DarkGray;
+        break;
+    case 13:
+        Console::ForegroundColor = ConsoleColor::Cyan;
+        break;
+    case 14:
+        Console::ForegroundColor = ConsoleColor::Magenta;
+        break;
+    }
+}
 
 void Dibujar_horario(int a, int b, int espera) {
     for (int i = a; i <= SCREEN_WIDTH; i++) {
